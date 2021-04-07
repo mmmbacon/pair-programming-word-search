5eabe05efe30774c9e9f9866ebfdb974912fe3fe
Brandon <bmacdonald1986@gmail.com>
2021-04-07 22:38:45 +0000

    completed wordSearch function

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,42 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transposeArray = function(matrix) {
+  const result = [];
+  for (let y = 0; y < matrix[0].length; y++) {
+    const tempArr = [];
+    for (let x = 0; x < matrix.length; x++) {
+      tempArr.push(matrix[x][y]);
     }
-}
+    result.push(tempArr);
+  }
+  return result;
+};
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+  let result = false;
+
+  //Horizontal Case
+  //Collapses array into a single value array containing a string
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  //Iterate through array values in horizontalJoin and check for letter
+  for (let l of horizontalJoin) {
+    //Find exact match in string
+    if (l.includes(word)) {
+      result = true;
+    } else {
+      let transposed = transposeArray(letters);
+      const verticalJoin = transposed.map(ls => ls.join(''));
+      //Iterate through array values in horizontalJoin and check for letter
+      for (let l of verticalJoin) {
+        //Find exact match in string
+        if (l.includes(word)) {
+          result = true;
+        } else {
+          let transposed = transposeArray(letters);
+          
+        }
+      }
+    }
+  }
+  return result;
+};
+
+module.exports = wordSearch;
